3cc400eb275190a326a312c598e1ebcde0506baf
PCF Developer <pcfuser@labbuildr.com>
2019-03-01 10:40:25 +0100

    added exits test

--- install-cf-uaac.ps1
+++ install-cf-uaac.ps1
@@ -1,7 +1,7 @@
 
 <#PSScriptInfo
 
-.VERSION 0.3
+.VERSION 0.4
 
 .GUID 50c63250-0c33-484e-a482-b76569b2f52c
 
@@ -45,9 +45,11 @@ $Download_Links = $request.Links | where class -match downloadlink
 $Devkits = $Download_Links | where {$_.innerHTML -Match [Regex]::Escape("Ruby+Devkit 2.5")}
 $Downloadlink = ($Devkits | where href -Match x64 | Select-Object -First 1).href
 $downloadfile = "$HOME/Downloads/$(Split-Path -Leaf $Downloadlink)"
-Invoke-WebRequest -UseBasicParsing -Uri $Downloadlink -OutFile $downloadfile
+if (!(Test-Path -PathType Leaf $downloadfile)) {
+    Invoke-WebRequest -UseBasicParsing -Uri $Downloadlink -OutFile $downloadfile
+}
 Start-Process $DownloadFile -ArgumentList '/SILENT /Dir="C:\Ruby25" /Components="ruby,msys2" /Group="Ruby 2.5.3-1-x64 with MSYS2" SetupType=custom' -Wait
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
 gem uninstall eventmachine --force
 gem install eventmachine --platform ruby
 gem install cf-uaac
